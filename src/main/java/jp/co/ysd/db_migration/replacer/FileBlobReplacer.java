8cb54e863b544cfdb5f7edf89751e9d7a2985a16
yuichi <ys.development0601@gmail.com>
2019-01-17 18:37:24 +0900

    blobカラムにファイルのバイナリデータをインサートできるように機能拡張した

---
+++ src/main/java/jp/co/ysd/db_migration/replacer/FileBlobReplacer.java
@@ -0,0 +1,36 @@
+package jp.co.ysd.db_migration.replacer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Types;
+
+import org.springframework.jdbc.core.SqlParameterValue;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author yuichi
+ *
+ */
+@Component
+public class FileBlobReplacer implements DataReplacer {
+
+	@Override
+	public Object replace(Object original) {
+		if (original instanceof String) {
+			String str = original.toString();
+			if (str.startsWith("file:")) {
+				String filePath = str.replaceAll("file:", "");
+				try {
+					original = new SqlParameterValue(Types.BLOB, Files.readAllBytes(new File(filePath).toPath()));
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+		}
+		return original;
+	}
+
+}

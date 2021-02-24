6c8f89fc38b7faa14d9368c30bd1a5f555f9aa8d
yuichi <ys.development0601@gmail.com>
2021-02-24 23:17:08 +0900

    CURRENT_TIMESTAMPはDate型ではなくフォーマット後の文字列に置換する

--- src/main/java/jp/co/ysd/db_migration/replacer/CurrentTimestampReplacer.java
+++ src/main/java/jp/co/ysd/db_migration/replacer/CurrentTimestampReplacer.java
@@ -1,5 +1,7 @@
 package jp.co.ysd.db_migration.replacer;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
@@ -12,12 +14,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class CurrentTimestampReplacer implements DataReplacer {
 
+	private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
 	@Override
 	public Object replace(Object original) {
 		if (original instanceof String) {
 			String str = original.toString();
 			if (str.equals("CURRENT_TIMESTAMP")) {
-				original = new Date();
+				original = DF.format(new Date());
 			}
 		}
 		return original;

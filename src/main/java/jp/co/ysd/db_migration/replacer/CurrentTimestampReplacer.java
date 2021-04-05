f8cc0f6b8ba868cc78edde03c30a929852ef3641
yuichi <ys.development0601@gmail.com>
2021-04-06 03:54:06 +0900

    DBのタイムゾーンを指定してデータ投入できるように機能追加

--- src/main/java/jp/co/ysd/db_migration/replacer/CurrentTimestampReplacer.java
+++ src/main/java/jp/co/ysd/db_migration/replacer/CurrentTimestampReplacer.java
@@ -3,7 +3,10 @@ package jp.co.ysd.db_migration.replacer;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,9 +15,17 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
-public class CurrentTimestampReplacer implements DataReplacer {
+public class CurrentTimestampReplacer implements DataReplacer, InitializingBean {
 
 	private static final DateFormat DF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	@Value("${database.timezone:UTC}")
+	private String timezone;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		DF.setTimeZone(TimeZone.getTimeZone(timezone));
+	}
 
 	@Override
 	public Object replace(Object original) {

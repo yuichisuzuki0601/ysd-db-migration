e3035714404ce918b1fc09b228bab6b26caabeef
yuichi <ys.development0601@gmail.com>
2022-12-15 02:30:18 +0900

    汎用化

--- src/main/java/jp/co/ysd/db_migration/DbMigrationApplication.java
+++ src/main/java/jp/co/ysd/db_migration/DbMigrationApplication.java
@@ -17,19 +17,13 @@ import jp.co.ysd.db_migration.manager.CommandManager;
 @SpringBootApplication
 public class DbMigrationApplication {
 
-	public static void main(String... args) throws Exception {
-		_main("-mode", "dropall");
-		_main("-rootdir", "../mast/database/mast-web-service", "-datadir", "data-prod");
-		_main("-mode", "dataall", "-rootdir", "../mast/database/mast-web-service", "-datadir", "data-dev");
-	}
-
 	private static final Map<String, String> FIXED_PROPERTIES = new HashMap<>();
 	static {
 		FIXED_PROPERTIES.put("logging.level.org.springframework", "WARN");
 		FIXED_PROPERTIES.put("spring.main.allowCircularReferences", "true");
 	}
 
-	public static void _main(String... args) throws Exception {
+	public static void main(String... args) throws Exception {
 		FIXED_PROPERTIES.entrySet().stream().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		var cm = new CommandManager(args);
 		var app = new SpringApplication(DbMigrationApplication.class);
